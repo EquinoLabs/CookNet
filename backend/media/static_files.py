18c8f9ffeab4563fd9932694c79c1a4ebf837409
Lakshya Raghuwanshi <lakshya.raghuwanshi.03@gmail.com>
2025-08-27 08:00:47 +0530

    changes

--- backend/media/static_files.py
+++ backend/media/static_files.py
@@ -4,5 +4,5 @@ from fastapi.staticfiles import StaticFiles
 
 def mount_static_files(app: FastAPI):
     app.mount("/media/images", StaticFiles(directory="media/images"), name="images")
-    app.mount("/media/icons", StaticFiles(directory="media/icons"), name="icons")
-    app.mount("/media/videos", StaticFiles(directory="media/videos"), name="videos")
+    # app.mount("/media/icons", StaticFiles(directory="media/icons"), name="icons")
+    # app.mount("/media/videos", StaticFiles(directory="media/videos"), name="videos")
