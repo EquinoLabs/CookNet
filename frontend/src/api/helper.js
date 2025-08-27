6f131c7c912520e9458e181952cc414afc29a37c
Lakshya Raghuwanshi <lakshya.raghuwanshi.03@gmail.com>
2025-08-27 08:13:53 +0530

    changes

--- frontend/src/api/helper.js
+++ frontend/src/api/helper.js
@@ -1,7 +1,9 @@
 // axios wrapper
 import axios from 'axios';
 
-const API_URL = import.meta.env.BACKEND_API_URL || '/api';
+const BACKEND_URL = import.meta.env.BACKEND_API_URL || '';
+const API_URL = `${BACKEND_URL}/api`;
+console.log('BACKEND_URL', API_URL);
 
 const api = axios.create({
   baseURL: API_URL,
