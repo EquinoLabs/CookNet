2e2c71b2f606f7512c07105fd62d9410d489e408
Lakshya Raghuwanshi <lakshya.raghuwanshi.03@gmail.com>
2025-08-27 07:24:46 +0530

    changes

--- backend/main.py
+++ backend/main.py
@@ -33,5 +33,5 @@ app.include_router(api_router, prefix="/api")
 mount_static_files(app)
 
 if __name__ == "__main__":
-    port = int(os.getenv("URL_PORT", 8080))
+    port = int(os.getenv("PORT", 8080))
     uvicorn.run(app, host="0.0.0.0", port=port, reload=True)
